6ba9093520fd9edd432e11714c536e062d303b67
TOV-003 <victortoba03@gmail.com>
2026-05-12 11:20:03 +0100

    Added Search Functionality to Navbar and Updated Home Page Title

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -92,7 +92,7 @@ export default function Home() {
                 </div>
             </div>
             <div className="p-4 flex flex-col items-center justify-center gap-12">
-                <h2 className="text-4xl font-bold text-white mb-4">Random Games</h2>
+                <h2 className="text-4xl font-bold text-white mb-4">Explore</h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
                     {randomGames && splitRandom ? randomGames.map((el) => (
                         <div key={el.id}>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,47 @@
 import { Link, NavLink } from "react-router-dom"
-import { useState } from "react"
+import { useState, useEffect, useContext } from "react"
+import { GameContext } from "../Context/GameContext"
+
 
 
 export default function Navbar() {
+    const [search, setSearch] = useState("");
     const [searchVisible, setSearchVisible] = useState(false);
+    const [searchResults, setSearchResults] = useState("");
+
+    const { apiKey } = useContext(GameContext);
+
+    useEffect(() => {
+        async function searchGames() {
+            if (searchVisible) {
+                try {
+                    const response = await fetch(
+                        `https://api.rawg.io/api/games?key=${apiKey}&search=${search}&page_size=5`
+                    );
+                    const data = await response.json();
+                    setSearchResults(data.results);
+                } catch (err) {
+                    console.error("Random Games Load Failed", err);
+                }
+            }
+
+        }
+
+        searchGames();
+    }, [search, searchVisible, apiKey]);
     return (
         <nav className="bg-digilore-primary text-white p-4 border-b border-white flex flex-col items-center md:flex-row md:justify-between gap-4">
-            <div className="bg-digilore-primary border border-white rounded-md flex flex-col w-80"></div>
+            <div className={`bg-digilore-primary border border-white rounded-md flex flex-col w-80 h-fit text-2xl absolute top-30 lg:top-18 lg:left-42 ${searchVisible ? 'block' : 'hidden'}`}>
+                {searchResults && searchResults.length > 0 ? (
+                    searchResults.map((result) => (
+                        <Link to={`/game/${result.id}`} className="text-white hover:text-digilore-secondary p-2 text-lg border-b border-white" key={result.id} onClick={() => setSearchVisible(false)}>
+                            {result.name}
+                        </Link>
+                    ))
+                ) : (
+                    <p className="text-gray-300">No games found.</p>
+                )}
+            </div>
             <div className="flex flex-col md:flex-row gap-4 md:gap-2.5 items-center w-full md:w-auto">
                 <Link to="/"><h1 className="text-xl font-bold">TITLE STACK</h1></Link>
 
@@ -16,6 +51,12 @@ export default function Navbar() {
                         type="text"
                         placeholder="Search..."
                         className="bg-transparent outline-none text-white placeholder:text-gray-400 w-full"
+                        value={search}
+                        onChange={(e) => {
+                            setSearch(e.target.value);
+                            setSearchVisible(true);
+                        }
+                        }
                     />
                 </div>
             </div>
